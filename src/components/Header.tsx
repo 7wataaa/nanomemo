e14a836e661204307944794347284d5f52cf6852
nashi <nn.contact.n@gmail.com>
2021-01-13 15:48:53 +0900

    add: AppBar
    ページの上部で検索・アカウント設定を行うため

---
+++ src/components/Header.tsx
@@ -0,0 +1,98 @@
+import { fade, AppBar, makeStyles, Toolbar, InputBase, Avatar, Typography } from '@material-ui/core'
+import { Person, Search } from '@material-ui/icons'
+import React from 'react'
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1
+  },
+  appbar: {
+    alignItems: 'start',
+  },
+  toolbar: {
+    width: '100%',
+    paddingRight: '24px',
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: '100%',
+    },
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  avatar: {
+    marginLeft: 'auto',
+    marginRight: '7%',
+    [theme.breakpoints.up('sm')]: {
+      marginRight: '50px',
+    },
+  }
+}))
+
+const Header = () => {
+
+  const classes = useStyles()
+
+  const time = Date()
+
+  console.log(time)
+
+  return <div className={classes.grow}>
+    <AppBar className={classes.appbar}>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant='h6' className={classes.title}>memoapp</Typography>
+        <div className={classes.search}>
+          <Search className={classes.searchIcon} />
+          <InputBase
+            placeholder="Search..."
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+          />
+        </div>
+        <Avatar className={classes.avatar}>
+          <Person />
+        </Avatar>
+      </Toolbar>
+    </AppBar>
+  </div>
+}
+
+export default Header
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,67 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDom from 'react-dom'
 
+import Btn from '@material-ui/core/Button'
+import { Fab, makeStyles, Card, createMuiTheme, colors, Theme, ThemeProvider } from '@material-ui/core'
+import Header from './components/Header'
+import { Add } from '@material-ui/icons'
+
+
+
+const useStyles = makeStyles((theme) => ({
+  offset: {
+    ...theme.mixins.toolbar,
+    //flexGrow: 1,
+  },
+  fab: {
+    margin: '0px',
+    top: 'auto',
+    right: '50px',
+    bottom: '25px',
+    left: 'auto',
+    position: 'fixed',
+  },
+}))
+
 function App() {
+  useEffect(() => {
+    console.log('123')
+  })
+
+  const classes = useStyles()
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#273944'
+      }
+    }
+  })
+
+  return <>
+    <ThemeProvider theme={theme}>
+      <Header />
+      <div className={classes.offset} />
+
+      <Body />
+
+      <Fab className={classes.fab}>
+        <Add />
+      </Fab>
+    </ThemeProvider>
+  </>
+}
+
+function Body() {
   const [count, setCount] = useState(0)
 
-  return <div></div>
+  return <>
+    <Card>a</Card>
+    <p>{count}回</p>
+    <Btn variant="contained" onClick={() => setCount(count + 1)}>
+      Click
+    </Btn>
+  </>
 }
 
 ReactDom.render(
