5a8b9f65b680fa87c64f48523a2c92f9fb1b14f4
nashi <nn.contact.n@gmail.com>
2021-01-14 10:08:34 +0900

    add: theme
    色設定を追加

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react'
 import ReactDom from 'react-dom'
 
 import Btn from '@material-ui/core/Button'
-import { Fab, makeStyles, Card, createMuiTheme, colors, Theme, ThemeProvider } from '@material-ui/core'
+import { Fab, makeStyles, Card, createMuiTheme, colors, Theme, ThemeProvider, CssBaseline, Typography, CardContent } from '@material-ui/core'
 import Header from './components/Header'
 import { Add } from '@material-ui/icons'
+import { myTheme } from "./theme";
 
 
-
-const useStyles = makeStyles((theme) => ({
+const useAppStyles = makeStyles((theme) => ({
   offset: {
     ...theme.mixins.toolbar,
     //flexGrow: 1,
@@ -28,18 +28,11 @@ function App() {
     console.log('123')
   })
 
-  const classes = useStyles()
-
-  const theme = createMuiTheme({
-    palette: {
-      primary: {
-        main: '#273944'
-      }
-    }
-  })
+  const classes = useAppStyles()
 
   return <>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={myTheme}>
+      <CssBaseline />
       <Header />
       <div className={classes.offset} />
 
@@ -52,11 +45,34 @@ function App() {
   </>
 }
 
+const useBodyStyle = makeStyles((theme) => ({
+  tagnames: {
+    fontSize: 14,
+  }
+  , title: {
+    marginBottom: 12,
+  }
+}))
+
 function Body() {
   const [count, setCount] = useState(0)
 
+  const classes = useBodyStyle();
+
   return <>
-    <Card>a</Card>
+    <Card variant="outlined">
+      <CardContent>
+        <Typography className={classes.tagnames} color="textSecondary" gutterBottom>
+          #tag
+        </Typography>
+        <Typography className={classes.title} variant="h5" component="h2">
+          Title
+        </Typography>
+        <Typography variant="body2" component="p">
+          メモの内容
+        </Typography>
+      </CardContent>
+    </Card>
     <p>{count}回</p>
     <Btn variant="contained" onClick={() => setCount(count + 1)}>
       Click
@@ -64,7 +80,4 @@ function Body() {
   </>
 }
 
-ReactDom.render(
-  <App />,
-  document.getElementById('root')
-)
+ReactDom.render(<App />, document.getElementById('root'))
